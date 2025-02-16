10cc8aa95d33704a03634c88fcb7cf887a6e0430
boommanpro <boommanpro@gmail.com>
2025-02-16 21:14:15 +0800

    feat(节点样式修改):

---
+++ src/components/ruleEngine/core/AiNodeToolBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <NodeToolbar :is-visible="true" :position="Position.Top">
+    <div class="tf-node-toolbar">
+      <el-button link>
+        <RunIcon/>
+      </el-button>
+      <el-button link>
+        <CopyIcon/>
+      </el-button>
+      <el-button link @click="deleteNode">
+        <DeleteIcon/>
+      </el-button>
+    </div>
+  </NodeToolbar>
+</template>
+<script setup lang="ts">
+import {Position} from "@vue-flow/core";
+import DeleteIcon from "@/components/ruleEngine/icons/DeleteIcon.vue";
+import {NodeToolbar} from "@vue-flow/node-toolbar";
+import RunIcon from "@/components/ruleEngine/icons/RunIcon.vue";
+import CopyIcon from "@/components/ruleEngine/icons/CopyIcon.vue";
+
+const deleteNode = (id) => {
+  console.log(id)
+}
+</script>
--- src/components/ruleEngine/core/Demo.vue
+++ src/components/ruleEngine/core/Demo.vue
@@ -6,6 +6,7 @@ import RunIcon from "@/components/ruleEngine/icons/RunIcon.vue";
 import CopyIcon from "@/components/ruleEngine/icons/CopyIcon.vue";
 import DeleteIcon from "@/components/ruleEngine/icons/DeleteIcon.vue";
 import DefinedParameterItem from "@/components/ruleEngine/core/base/DefinedParameterItem.vue";
+import AiNodeToolBar from "@/components/ruleEngine/core/AiNodeToolBar.vue";
 
 const props = defineProps({
   data: {
@@ -26,18 +27,6 @@ const sourceConnections = useHandleConnections({
 
 const targetConnections = useHandleConnections({
   type: 'source',
-})
-
-const isSender = toRef(() => sourceConnections.value.length <= 0)
-
-const isReceiver = toRef(() => targetConnections.value.length <= 0)
-
-
-const processLabel = computed(() => {
-  if (props.data.hasError) {
-    return '❌'
-  }
-  return '🏠'
 })
 
 const deleteNode = (id) => {
@@ -63,44 +52,106 @@ const addParam = () => {
 </script>
 
 <template>
-  <div class="process-node">
-
-    <NodeToolbar :is-visible="true" :position="Position.Top">
-      <div class="tf-node-toolbar">
-        <el-button link>
-          <RunIcon/>
-        </el-button>
-
-        <el-button link>
-          <CopyIcon/>
-        </el-button>
-        <el-button link @click="deleteNode(data)">
-          <DeleteIcon/>
-        </el-button>
+  <div class="ai-flow__nodes">
+    <div class=".ai-flow__node">
+      <ai-node-tool-bar/>
+      <div class="tf-node-wrapper">
+        <div class="tf-node-wrapper-title">
+          vue-flow-rule-engine
+        </div>
+        <div class="tf-node-wrapper-body">
+          <h3>{{ node.id }}</h3>
+          <defined-parameter-item/>
+        </div>
       </div>
-    </NodeToolbar>
-
-    <Handle v-if="!isSender" type="target" :position="targetPosition">
-      <span
-          v-if="!data.isRunning && !data.isFinished && !data.isCancelled && !data.isSkipped && !data.hasError">📥 </span>
-    </Handle>
-
-    <Handle type="source" :position="sourcePosition"/>
-
-    <div class="node">
-      <h3>{{ node.id }}</h3>
-      <defined-parameter-item/>
-
     </div>
-
-    <span>
-      {{ processLabel }}
-    </span>
   </div>
 </template>
 
 
-<style scoped>
+<style lang="scss" scoped>
+.tf-node-wrapper {
+  border-radius: 5px;
+  min-width: 300px;
+  background: #fff;
 
+  &-title {
+    height: 30px;
+    background: #EFF1F5;
+    color: #bcbcbc;
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+    padding-left: 5px;
+    border-bottom: 1px solid #ccc;
+    font-weight: 300;
+    letter-spacing: 1px;
+  }
 
+  &-body {
+    padding: 10px;
+  }
+}
+
+.ai-flow__nodes {
+  .ai-flow__node {
+    border: 3px solid transparent;
+    border-radius: 5px;
+    box-sizing: border-box;
+
+    .ai-flow__handle {
+      width: 16px;
+      height: 16px;
+      background: transparent;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: none;
+
+      &::after {
+        content: ' ';
+        background: #2563EB;
+        width: 8px;
+        height: 8px;
+        border-radius: 100%;
+        transition: width 0.1s, height 0.1s;
+      }
+
+      &:hover {
+        &::after {
+          width: 16px;
+          height: 16px;
+        }
+      }
+    }
+
+    &:after {
+      content: ' ';
+      position: absolute;
+      border-radius: 5px;
+      top: -2px;
+      left: -2px;
+      border: 1px solid #ccc;
+      height: calc(100% + 2px);
+      width: calc(100% + 2px);
+    }
+
+    &:hover {
+      border: 3px solid #bacaef7d;
+    }
+
+    &.selectable.selected {
+      border: 3px solid #bacaef7d;
+      box-shadow: var(--xy-node-boxshadow-selected);
+    }
+
+    &:hover:after {
+      display: none;
+    }
+
+    &.selectable.selected:after {
+      display: none;
+    }
+  }
+}
 </style>
