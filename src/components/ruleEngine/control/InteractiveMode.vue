a45768c5392379bf538b4f98e296725babac0b59
boommanpro <boommanpro@gmail.com>
2025-03-01 00:52:35 +0800

    样式实现切换为tailwindcss

--- src/components/ruleEngine/control/InteractiveMode.vue
+++ src/components/ruleEngine/control/InteractiveMode.vue
@@ -7,21 +7,21 @@
     <div v-if="isDropdownOpen"
          class="interactive-mode absolute bottom-full left-0 bg-base-100 p-4 shadow-lg rounded-box z-10 mb-2">
       <div>
-        <h4 class="p-2 interactive-title">交互模式</h4>
+        <h4 class="p-2 font-semibold text-2xl">交互模式</h4>
         <div class="flex gap-4">
-          <div class="interactive-button">
+          <div class="bg-white border border-gray-200 rounded-lg p-4 text-center items-center justify-center cursor-pointer hover:bg-blue-50 hover:border-blue-400 w-60">
             <div class="p-4 flex items-center justify-center">
               <CustomIcon name="mouse-max" />
             </div>
-            <h6 class="interactive-sub-title">鼠标友好模式</h6>
-            <p class="text-gray-500 interactive-sub-description">鼠标左键拖动画布，滚轮缩放</p>
+            <h6 class="font-semibold text-lg">鼠标友好模式</h6>
+            <p class="text-gray-500 ">鼠标左键拖动画布，滚轮缩放</p>
           </div>
-          <div class="interactive-button">
+          <div class="bg-white border border-gray-200 rounded-lg p-4 text-center items-center justify-center cursor-pointer hover:bg-blue-50 hover:border-blue-400 w-60">
             <div class="p-4 flex items-center justify-center">
               <CustomIcon name="touch-max" />
             </div>
-            <h6 class="interactive-sub-title">触控板友好模式</h6>
-            <p class="text-gray-500 interactive-sub-description">双指同向移动拖动，双指张开捏合缩放</p>
+            <h6 class="font-semibold text-lg">触控板友好模式</h6>
+            <p class="text-gray-500 ">双指同向移动拖动，双指张开捏合缩放</p>
           </div>
         </div>
       </div>
@@ -41,37 +41,5 @@ const toggleDropdown = () => {
 </script>
 
 <style scoped>
-/* 你可以在这里添加额外的样式 */
-.interactive-title{
-  font-family: PingFang SC, Noto Sans SC, sans-serif;
-    font-size: 20px;
-    font-weight: 600;
-    line-height: 28px;
-    margin: 0;
-}
-.interactive-button-clicked{
-  background-color: var(--coz-mg-hglt, rgba(186, 192, 255, .2));
-    border: 1px solid var(--coz-stroke-hglt, #4e40e5);
-    border-radius: var(--default, 8px);
-    cursor: pointer;
-}
-.interactive-button{
-  background: var(--coz-mg-card, #fff);
-    border: 1px solid var(--coz-stroke-plus, rgba(6, 7, 9, .15));
-    border-radius: var(--default, 8px);
-    box-sizing: border-box;
-    padding-bottom: 20px;
-    text-align: center;
-    width: 220px;
-}
-.interactive-sub-title{
-  font-family: PingFang SC, Noto Sans SC, sans-serif;
-    font-size: 16px;
-    font-weight: 600;
-    line-height: 22px;
-    margin: 0;
-}
-.interactive-sub-description{
-  padding: 4px 12px 0;
-}
+
 </style>
